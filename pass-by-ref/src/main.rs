7598b438cb734f67169447084a764d3795c5f838
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2022-08-31 01:24:37 +0530

    pass by reference

---
+++ pass-by-ref/src/main.rs
@@ -0,0 +1,23 @@
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8
+}
+
+fn main() {
+    
+    let blue: Color = Color { red: 0, green: 0, blue: 255 };
+
+    print_color(&blue);
+    print_color(&blue);
+
+    /* 
+    print_color(blue);
+    print_color(&blue);
+    // -- Gives error
+    */
+}
+
+fn print_color(c: &Color) {
+    println!("Color - R:{}, G:{}, B:{}", c.red, c.green, c.blue);
+}
