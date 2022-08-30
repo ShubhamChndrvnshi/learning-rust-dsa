4555875b1e18b46588c1f409314c385d94eaf45a
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2022-08-31 01:31:33 +0530

    arrays

---
+++ arrays/Readme.md
@@ -0,0 +1,4 @@
+## Arrays
+
+- Sequence of values that can be accessed using their indexes.
+- The data types have to homogeneous.
---
+++ arrays/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+
+    let nums: [u32; 5] = [1, 4, 2, 3, 5];
+
+    for i in 0..nums.len() {
+        println!("{}", i);
+    }
+}
