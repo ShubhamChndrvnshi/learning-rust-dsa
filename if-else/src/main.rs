6044f343ac4813d1256a104d28dd6a323f36eea8
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2022-08-30 17:35:40 +0530

    if-else

---
+++ if-else/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let i: u8 = 34;
+
+    if i> 0 {
+        println!("num is +ve");
+    } 
+    else if i == 0 {
+        println!("num is 0");
+    }
+    else {
+        println!("num is -ve");
+    }
+}
