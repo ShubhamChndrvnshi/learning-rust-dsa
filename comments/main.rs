c03d3976c2349e3eceaf01d41bc66f9a65a1a39b
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2022-08-30 16:43:07 +0530

    comments

---
+++ comments/main.rs
@@ -0,0 +1,9 @@
+fn main(){
+    // This is a single line comment example
+    /*
+        this is multi-
+            line comment
+        it's ignored by compiler
+    */
+    printl!("Hello world");
+}
